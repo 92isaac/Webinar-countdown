cda30c82abd21a5e40e7f82e07af4b2f7fffc517
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-07-02 19:39:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Subscribe from './component/Subscribe';
+import Countd from './component/Countd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Subscribe /> } />
+          <Route path="/webinar" element={<Countd /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
